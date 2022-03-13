f213933553003cb83159238435671554d4b60fbf
Larson-09 <jordan.laude.9@gmail.com>
2022-03-13 11:31:33 +0100

    add some colors and string ressources

---
+++ app/src/main/java/fr/larson/perftracker/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.larson.perftracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
